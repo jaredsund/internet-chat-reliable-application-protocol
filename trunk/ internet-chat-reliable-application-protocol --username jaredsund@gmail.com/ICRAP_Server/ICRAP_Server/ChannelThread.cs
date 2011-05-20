6235c0e45bce9ec497bccd71e333d0dba2b644e7
jaredsund@gmail.com <jaredsund@gmail.com@7b784fec-2d87-79a1-d7cf-9a0647488bf5>
2011-05-20 10:25:49 +0000

    Added tests and new server

---
+++ trunk/ internet-chat-reliable-application-protocol --username jaredsund@gmail.com/ICRAP_Server/ICRAP_Server/ChannelThread.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace ICRAP_Server
+{
+    class ChannelThread
+    {
+        private BackgroundWorker worker;
+        private System.Windows.Forms.ListBox  listbox;
+        public Int32 port;
+        public string ChannelName;
+
+        public ChannelThread(ref System.Windows.Forms.ListBox listbox, string ChannelName)
+        {
+            this.listbox = listbox;
+            this.ChannelName = ChannelName;
+            
+
+            worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+            worker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.worker_DoWork);
+            worker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.worker_ProgressChanged);
+            worker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.worker_RunWorkerCompleted);
+            worker.RunWorkerAsync();
+        }
+
+        public void killThread()
+        {
+            worker.CancelAsync();
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Get the BackgroundWorker object that raised this event.
+            System.ComponentModel.BackgroundWorker worker2;
+            worker2 = (System.ComponentModel.BackgroundWorker)sender;
+
+            Channel myChannel = new Channel();
+            myChannel.startChannel(worker2, e, ChannelName );
+            
+        }
+
+        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            Channel.CurrentState state = (Channel.CurrentState)e.UserState;
+
+            port = state.port;
+
+            listbox.Items.Add(String.Format("{0}, {1}: {2}", port.ToString(), state.channelName, state.message));
+
+        }
+
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            listbox.Items.Add("done");
+        }
+
+    }
+}
--- trunk/ internet-chat-reliable-application-protocol --username jaredsund@gmail.com/ICRAP_Server/ICRAP_Server/Form1.Designer.cs
+++ trunk/ internet-chat-reliable-application-protocol --username jaredsund@gmail.com/ICRAP_Server/ICRAP_Server/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace TCP_Client_Test
+﻿namespace ICRAP_Server
 {
     partial class Form1
     {
@@ -28,90 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // label1
+            // listBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 82);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 50);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(598, 186);
+            this.listBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(197, 23);
+            this.button1.Location = new System.Drawing.Point(37, 21);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(16, 23);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(175, 20);
-            this.textBox1.TabIndex = 2;
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(197, 52);
+            this.button2.Location = new System.Drawing.Point(118, 21);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(278, 23);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 4;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(278, 55);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(431, 262);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.ClientSize = new System.Drawing.Size(622, 278);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "ICRAP Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
